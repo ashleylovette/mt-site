1eca9228e31125843eb986ccd0d33d6da0ed3e4a
ashleylovette <ashleylovette15@gmail.com>
2024-10-28 19:05:52 -0500

    section one and two init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SectionOneComponent } from './section-one/section-one.component';
+import { SectionTwoComponent } from './section-two/section-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SectionOneComponent,
+    SectionTwoComponent
   ],
   imports: [
     BrowserModule,
